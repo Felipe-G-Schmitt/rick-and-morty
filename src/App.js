aed693c60f85a53c1f80d81af9cd1118eb315656
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2023-04-10 14:54:20 -0300

    shgsfsdds

--- src/App.js
+++ src/App.js
@@ -1,84 +1,10 @@
 import "./css/App.css";
 import { useEffect, useState } from "react";
 
-const mock = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    type: "",
-    gender: "Male",
-    origin: {
-      name: "Earth (C-137)",
-      url: "https://rickandmortyapi.com/api/location/1",
-    },
-    location: {
-      name: "Citadel of Ricks",
-      url: "https://rickandmortyapi.com/api/location/3",
-    },
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    episode: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2",
-      "https://rickandmortyapi.com/api/episode/3",
-      "https://rickandmortyapi.com/api/episode/4",
-      "https://rickandmortyapi.com/api/episode/5",
-      "https://rickandmortyapi.com/api/episode/6",
-      "https://rickandmortyapi.com/api/episode/7",
-      "https://rickandmortyapi.com/api/episode/8",
-      "https://rickandmortyapi.com/api/episode/9",
-      "https://rickandmortyapi.com/api/episode/10",
-      "https://rickandmortyapi.com/api/episode/11",
-      "https://rickandmortyapi.com/api/episode/12",
-      "https://rickandmortyapi.com/api/episode/13",
-      "https://rickandmortyapi.com/api/episode/14",
-      "https://rickandmortyapi.com/api/episode/15",
-      "https://rickandmortyapi.com/api/episode/16",
-      "https://rickandmortyapi.com/api/episode/17",
-      "https://rickandmortyapi.com/api/episode/18",
-      "https://rickandmortyapi.com/api/episode/19",
-      "https://rickandmortyapi.com/api/episode/20",
-      "https://rickandmortyapi.com/api/episode/21",
-      "https://rickandmortyapi.com/api/episode/22",
-      "https://rickandmortyapi.com/api/episode/23",
-      "https://rickandmortyapi.com/api/episode/24",
-      "https://rickandmortyapi.com/api/episode/25",
-      "https://rickandmortyapi.com/api/episode/26",
-      "https://rickandmortyapi.com/api/episode/27",
-      "https://rickandmortyapi.com/api/episode/28",
-      "https://rickandmortyapi.com/api/episode/29",
-      "https://rickandmortyapi.com/api/episode/30",
-      "https://rickandmortyapi.com/api/episode/31",
-      "https://rickandmortyapi.com/api/episode/32",
-      "https://rickandmortyapi.com/api/episode/33",
-      "https://rickandmortyapi.com/api/episode/34",
-      "https://rickandmortyapi.com/api/episode/35",
-      "https://rickandmortyapi.com/api/episode/36",
-      "https://rickandmortyapi.com/api/episode/37",
-      "https://rickandmortyapi.com/api/episode/38",
-      "https://rickandmortyapi.com/api/episode/39",
-      "https://rickandmortyapi.com/api/episode/40",
-      "https://rickandmortyapi.com/api/episode/41",
-      "https://rickandmortyapi.com/api/episode/42",
-      "https://rickandmortyapi.com/api/episode/43",
-      "https://rickandmortyapi.com/api/episode/44",
-      "https://rickandmortyapi.com/api/episode/45",
-      "https://rickandmortyapi.com/api/episode/46",
-      "https://rickandmortyapi.com/api/episode/47",
-      "https://rickandmortyapi.com/api/episode/48",
-      "https://rickandmortyapi.com/api/episode/49",
-      "https://rickandmortyapi.com/api/episode/50",
-      "https://rickandmortyapi.com/api/episode/51",
-    ],
-    url: "https://rickandmortyapi.com/api/character/1",
-    created: "2017-11-04T18:48:46.250Z",
-  },
-];
-
 function App() {
-  const [conteudo, setConteudo] = useState(<></>)
-
+  const [conteudo, setConteudo] = useState(<></>);
+  // o setBusca e setConteudo são funções que dão valor para as variáveis, o useState é uma função que cria variáveis. 
+  //O conteudo é a variável que vai receber o valor da função setConteudo, e o busca é a variável que vai receber o valor da função setBusca
   const [busca, setBusca] = useState("");
 
   function getStatus(status) {
@@ -158,6 +84,32 @@ function App() {
     ));
   }
 
+  function montarFiltro(tipo, valor) {
+    const filtros = new URLSearchParams();
+    /* 
+    * filtros = {
+    }
+    */
+
+    const batata = filtros.get(tipo)
+    if (retorno === valor){
+      filtros.delete(tipo)
+    } else{
+      filtros.set(tipo, valor)
+    }
+
+    filtros.set(tipo, valor)
+    /* 
+    * filtros = {
+    *   status: 'alive'
+    }
+    */
+
+    setBusca('?'+filtros.toString())
+  }
+
+
+
   useEffect(() => {
     async function getConteudo(){
       setConteudo(await listaPersonagem());
@@ -176,7 +128,7 @@ function App() {
         <span className="titulo-filtros">Filtros</span>
         <div className="filtro status">
           <b>Status:</b>
-          <span onClick={() => setBusca('?status=alive')}>Vivo</span>
+          <span onClick={() => montarFiltro('status', 'alive')}>Vivo</span>
           <span onClick={() => setBusca('?status=dead')}>Morto</span>
           <span onClick={() => setBusca('?status=unknown')}>Desconhecido</span>
         </div>
@@ -184,7 +136,7 @@ function App() {
           <b>Gênero:</b>
           <span onClick={() => setBusca('?gender=male')}>Masculino</span>
           <span onClick={() => setBusca('?gender=female')}>Feminino</span>
-          <span>Sem gênero</span>
+          <span onClick={() => setBusca('?gender=genderless')}>Sem gênero</span>
           <span onClick={() => setBusca('?gender=unknown')}>Desconhecido</span>
         </div>
       </div>
